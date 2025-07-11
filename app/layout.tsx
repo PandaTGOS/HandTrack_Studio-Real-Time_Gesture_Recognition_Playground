68246fdf86e21e360b3f7a0b2a1a25b64a3f7691
Sakhi Saswat Panda <113613706+PandaTGOS@users.noreply.github.com>
2025-07-12 00:33:22 +0530

    fixing_deployment

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import Script from "next/script"
 import "./globals.css";
 
 const geistSans = Geist({
@@ -24,12 +25,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        <script src="https://cdn.jsdelivr.net/npm/@mediapipe/hands/hands.js"></script>
-        <script src="https://cdn.jsdelivr.net/npm/@mediapipe/camera_utils/camera_utils.js"></script>
-
+      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+        <Script src="https://cdn.jsdelivr.net/npm/@mediapipe/hands/hands.js" strategy="beforeInteractive"/>
+        <Script src="https://cdn.jsdelivr.net/npm/@mediapipe/camera_utils/camera_utils.js" strategy="beforeInteractive"/>
         {children}
       </body>
     </html>
